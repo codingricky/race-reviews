f21f190c21248d010589ffb94d9580ea394a5669
Ricky <ricky@dius.com.au>
2013-12-26 10:54:39 +1100

    added initial date support

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('app', ['ngResource', 'ngRoute', 'app.services', 'app.controllers'])
+angular.module('app', ['ui.bootstrap.datetimepicker', 'ngResource', 'ngRoute', 'app.services', 'app.controllers'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/index', {templateUrl: 'assets/angular/index.html', controller: 'tableRaceController'})
             .when('/race/:id', {templateUrl: 'assets/angular/details.html', controller: 'raceController'})
